6703618aea0855dfc5184524fa8654409b290ad1
Sergey Stoyko <sergeysinoptik@gmail.com>
2021-11-28 17:14:30 +0300

    form validation

--- src/js/script.js
+++ src/js/script.js
@@ -8,11 +8,6 @@ function letAnswered(i) {
 
     let parent = target.parentElement;
     let children = parent.children;
-    
-
-    console.log(target, 'target')
-    console.log(question, 'question')
-    console.log(answer, 'answer')
 
     if (!question.classList.contains('answered')) {
         Array.from(children).forEach(e => e.firstElementChild.classList.remove('answered'));
@@ -34,3 +29,58 @@ function letAnswered(i) {
 }
 
 Array.from(singleFaqs).forEach(e => letAnswered(e));
+
+let textInput = document.querySelector('#text-input');
+let textLabel = document.getElementById('text-input-label');
+
+function textInputed(e) {
+    let input = document.querySelector('#text-input');
+    input.addEventListener('blur', function() {
+        if (input.value !== '') {
+            textLabel.style.visibility = 'hidden';
+            if (input.value.length < 3) {
+                input.classList.add('invalid');
+                input.classList.remove('correct');
+            } else {
+                input.classList.add('correct');
+                input.classList.remove('invalid');
+            }
+        } else {
+            textLabel.style.visibility = 'visible';
+        }
+    });
+    input.addEventListener('focus', function() {
+        textLabel.style.visibility = 'visible';
+        input.classList.remove('correct');
+        input.classList.remove('invalid');
+    });
+};
+
+let phoneInput = document.querySelector('#phone-input');
+let phoneLabel = document.getElementById('phone-input-label');
+
+function phoneInputed(e) {
+    let input = document.querySelector('#phone-input');
+    input.addEventListener('blur', function() {
+        if (input.value !== '') {
+            phoneLabel.style.visibility = 'hidden';
+            if ((input.value.length < 11 || (input.value.length > 11))) {
+                input.classList.add('invalid');
+                input.classList.remove('correct');
+            } else {
+                input.classList.add('correct');
+                input.classList.remove('invalid');
+            }
+        } else {
+            phoneLabel.style.visibility = 'visible';
+            input.classList.remove('correct');
+            input.classList.remove('invalid');
+        }
+    });
+    input.addEventListener('focus', function() {
+        phoneLabel.style.visibility = 'visible';;
+    });
+};
+
+textInput.oninput = textInputed;
+phoneInput.oninput = phoneInputed;
